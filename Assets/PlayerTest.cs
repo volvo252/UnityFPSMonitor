932c2d8ce63139d2e8b8ab89c86b7636da70ad69
Roman Porter <roma53591@yandex.ru>
2023-10-28 14:40:24 +0300

    playertest tru

---
+++ Assets/PlayerTest.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ну ладно, не соси
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
